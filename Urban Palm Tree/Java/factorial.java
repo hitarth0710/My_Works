7445241c4209ad27aa5f70d82b9456647fbc98ee
Hitarth <2022002462.gcet@cvmu.edu.in>
2024-02-10 12:35:53 +0530

    new

---
+++ Urban Palm Tree/Java/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = scanner.nextInt();
+        System.out.println("Factorial of " + num + " = " + factorial(num));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
