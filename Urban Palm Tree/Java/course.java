61c470fbba2de94287dc21011505cb4d866d47ea
Hitarth <hitarthsoni947@gmail.com>
2024-03-21 16:38:50 +0530

    new

---
+++ Urban Palm Tree/Java/course.java
@@ -0,0 +1,40 @@
+
+public class course {
+    public static void main(String[] args) {
+        String output = "";
+
+  int x = 2;
+
+  switch(x){
+
+  case 1:
+
+  case 2:
+
+  case 3:
+
+   output += "one,two,three,";
+
+  case 4:
+
+   output += "four,";
+
+   break;
+
+  case 5:
+
+   output += "five,";
+
+   break;
+
+  default:
+
+   output += "six";
+
+  output += "six";
+
+  }
+
+  System.out.println(output);
+    }
+}
