293be154019ac90f1dd3606d47e74b144edda896
Hitarth <hitarthsoni947@gmail.com>
2024-07-13 16:36:08 +0530

    minor changes in jdbc program

--- Programming/Advanced Java/src/Practical1_1.java
+++ Programming/Advanced Java/src/Practical1_1.java
@@ -1,16 +1,16 @@
 import java.sql.*;
 public class Practical1_1 {
     
+
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hitarth", "root","mysql");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("Select * from student");
-            
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + "\t|" + rs.getString(2) + "\t|" + rs.getString(3));
-            }
+            ResultSet rs = stmt.executeQuery("select * from student");
+           while (rs.next()) {
+                System.out.printf("%d\t| %s\t| %s%n", rs.getInt(1), rs.getString(2), rs.getString(3));
+           }
             con.close();
         } catch (Exception e) {
             System.out.println(e);
--- Programming/Advanced Java/src/Practical1_2.java
+++ Programming/Advanced Java/src/Practical1_2.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Practical1_2 {
+import java.sql.*;
+import java.util.Scanner;
+
+public class Practical1_2 {
+    public static final String URL = "jdbc:mysql://localhost:3306/hitarth";
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(URL,"root","mysql");
+
+        String Query = "insert into student values(?, ?, ?)";
+        PreparedStatement pstmt = con.prepareStatement(Query);
+
+        System.out.println("How many student's information you want to enter: ");
+        int n = sc.nextInt();
+        int [] id = new int[n];
+        String[] name = new String[n];
+        String[] city = new String[n];
+        for(int i=0;i<n;i++){
+              System.out.println("Enter the id of new student: ");
+              id[i] = sc.nextInt();
+              pstmt.setInt(1,id[i]);
+
+              sc.nextLine();
+
+              System.out.println("Enter the name of new student: ");
+              name[i] = sc.nextLine();
+              pstmt.setString(2,name[i]);
+              System.out.println("Enter the city of new student: ");
+              city[i] = sc.nextLine();
+              pstmt.setString(3,city[i]);
+              int afr = pstmt.executeUpdate();
+              System.out.println("Rows affected: "+afr);
+        }
+        sc.close();
+    }
 }
