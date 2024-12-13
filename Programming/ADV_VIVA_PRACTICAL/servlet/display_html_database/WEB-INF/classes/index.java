4ab9cebe12fa0546175fb43214890f9b55f680a5
Hitarth <144959770+hitarth0710@users.noreply.github.com>
2024-12-13 10:30:19 +0530

    Update index.java

--- Programming/ADV_VIVA_PRACTICAL/servlet/display_html_database/WEB-INF/classes/index.java
+++ Programming/ADV_VIVA_PRACTICAL/servlet/display_html_database/WEB-INF/classes/index.java
@@ -1,32 +1,30 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-
-public class index extends HttpServlet {
-
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        String name = req.getParameter("Name");
-        int age = Integer.parseInt(req.getParameter("Age"));
-        String email = req.getParameter("Email");
-        String contact = req.getParameter("Num");
-
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Login</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Welcome!</h1>");
-        out.println("<br>");
-        out.println("Name: " + name);
-        out.println("<br>");
-        out.println("Age: " + age);
-        out.println("<br>");
-        out.println("Email: " + email);
-        out.println("<br>");
-        out.println("Contact No.: " + contact);
-        out.println("</body>");
-        out.println("</html>");
-    }
-}
+import java.io.*; 
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+ 
+public class index extends HttpServlet { 
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException,  ServletException
+    { 
+        String operation = req.getParameter("operation"); 
+ 
+        switch (operation) { 
+            case "add": 
+                req.getRequestDispatcher("AddStudent").forward(req, res); 
+                break; 
+            case "edit": 
+                req.getRequestDispatcher("EditStudent").forward(req, res); 
+                break; 
+            case "delete": 
+                req.getRequestDispatcher("DeleteStudent").forward(req, res); 
+                break; 
+            case "search": 
+                req.getRequestDispatcher("SearchStudent").forward(req, res); 
+                break; 
+            case "viewAll": 
+                req.getRequestDispatcher("ViewAllStudents").forward(req, res); 
+                break; 
+             
+        } 
+    } 
+} 
